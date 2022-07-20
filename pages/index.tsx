1f65e4c9b94db5e5e21c7bb177ab37245cce5cbf
Alysson Barrera <alyssonbarrera@outlook.com>
2022-07-20 15:27:48 -0300

    :sparkles: Autenticação com SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+import { GetServerSideProps } from "next";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { parseCookies } from "nookies";
 import styles from "../styles/Home.module.css";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
 
@@ -26,3 +29,12 @@ export default function Home() {
       </form>
     )
 }
+
+export const getServerSideProps = withSSRGuest(async (context) => {
+
+  return {
+    props: {
+      
+    }
+  };
+})
---
+++ utils/withSSRAuth.ts
@@ -0,0 +1,34 @@
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
+import { destroyCookie, parseCookies } from "nookies";
+import { AuthTokenError } from "../services/errors/AuthTokenErro";
+
+export function withSSRAuth<P>(fn: GetServerSideProps<P>): GetServerSideProps {
+    return async (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
+        const cookies = parseCookies(context);
+
+        if(!cookies['nextauth.token']) {
+            return {
+                redirect: {
+                    destination: '/',
+                    permanent: false
+                }
+            }
+        }
+        try {
+            return await fn(context);
+        } catch (error) {
+            if(error instanceof AuthTokenError) {
+
+                destroyCookie(context, 'nextauth.token')
+                destroyCookie(context, 'nextauth.refreshToken')
+        
+                return {
+                    redirect: {
+                        destination: '/',
+                        permanent: false
+                    }
+                }
+            }
+        }
+    }
+}
